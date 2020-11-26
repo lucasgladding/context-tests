b5bda55770c7607fdbc6520e3884e79d2f3ac458
Lucas Gladding <l.gladding@vehikl.com>
2020-11-26 08:07:18 -0500

    Used login in example app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import AuthAPI from './apis/AuthAPI';
+import { AuthProvider } from './contexts/AuthContext';
+import Login from './components/Login';
+
 function App() {
+  const auth = new AuthAPI();
+
   return (
-    <div>
-      context-tests
-    </div>
+    <AuthProvider>
+      <Login api={auth} />
+    </AuthProvider>
   );
 }
 
